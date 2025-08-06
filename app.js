06021b329ceeac79f368083fe1a96a8729302359
Bella9693 <realbella9693@gmail.com>
2025-08-05 21:43:55 -0700

    exercise complete

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-$(function() {
+$(function () {
   // YOUR CODE HERE
-})
+  $("#toggleButton").on("click", function () {
+    $("#myParagraph").toggle();
+  });
+
+  $("#colorButton").on("click", function () {
+    $("#colorDiv").css("background-color", "yellow");
+  });
+
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").append("<div id='classDiv'></div>");
+  });
+  $("#removeClassButton").on("click", function () {
+    $("#classDiv").children("div").first().remove();
+  });
+
+  $("#fadeInButton").on("click", function () {
+    $("#fadeDiv").fadeIn();
+  });
+  $("#fadeOutButton").on("click", function () {
+    $("#fadeDiv").fadeOut();
+  });
+
+  $("#slideUpButton").on("click", function () {
+    $("#slideDiv").slideUp();
+  });
+  $("#slideDownButton").on("click", function () {
+    $("#slideDiv").slideDown();
+  });
+});
